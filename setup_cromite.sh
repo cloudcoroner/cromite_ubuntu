5cac2787f10182c509db20fd74ba1c924fb46192
cloudcoroner <93301474+cloudcoroner@users.noreply.github.com>
2024-12-19 15:36:53 -0500

    Update setup_cromite.sh

--- setup_cromite.sh
+++ setup_cromite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# setup_cromite.sh
+#setup_cromite.sh
 
 #export HISTIGNORE='*sudo -S*'
 
@@ -13,9 +13,7 @@ if [ "$(lsb_release -s -i)" = "Ubuntu" ]; then
 	
         if zenity --question --title "Cromite Install Tool" --width 500 --height 100 --text "This will install Cromite and set it as the default web browser!\nClick Yes to proceed with the installation  OR \nclick No to cancel."; then
         					
-        	password=$(zenity --forms --title "Cromite Install Tool" --width 500 --height 100 --text "Enter root password to install Cromite." --add-password "Password:")
-        	
-		#GitHub repo redirect for latest release
+  		#GitHub repo redirect for latest release
 		cromiteurllatestredirect="https://github.com/uazo/cromite/releases/latest"
 		
 		#GitHub release download repo
@@ -26,7 +24,10 @@ if [ "$(lsb_release -s -i)" = "Ubuntu" ]; then
 		
 		#follow redirect to get the latest release version
 		cromitelatest=$(wget --max-redirect=0 $cromiteurllatestredirect 2>&1 | awk '/Location: /,// { print }' | awk '{print $2}' | awk -F "/" '{print $NF}')
-		
+
+  		#get root password for install
+  		password=$(zenity --forms --title "Cromite Install Tool" --width 500 --height 100 --text "Enter root password to install Cromite." --add-password "Password:")
+        			
 		#create install directory
 		echo $password | sudo -S rm -rf /usr/bin/cromite
 		echo $password | sudo -S mkdir /usr/bin/cromite
@@ -42,7 +43,7 @@ if [ "$(lsb_release -s -i)" = "Ubuntu" ]; then
 		
 		#create file with installed version for future updates
 		echo $password | sudo -S touch /usr/bin/cromite/cromite.ver
-		echo $password | sudo -S echo $cromitelatest | tee /usr/bin/cromite/cromite.ver
+		echo $password | echo $cromitelatest | sudo -S tee /usr/bin/cromite/cromite.ver
 		
 		#download cromite startup file with update check and make executable
 		echo $password | sudo -S wget $cromitestartupurl -P /usr/bin/cromite/
@@ -59,26 +60,26 @@ if [ "$(lsb_release -s -i)" = "Ubuntu" ]; then
             
             	#create cromite desktop file
 		echo $password | sudo -S touch "/usr/share/applications/cromite.desktop"
-		echo $password | sudo -S 'echo "#!/bin/sh" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo '' | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "# Author:      SAEOS, saeos [dot] com" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "# Description: Cromite is a Chromium fork based on Bromite with built-in support for adblocking and an eye for privacy." | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "# Executed by: user, on-demand" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "# Resources:   /bin/bash, default Ubuntu icon set" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "#" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "[Desktop Entry]" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Name=Cromite" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "X-MultipleArgs=false" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Exec=/usr/bin/cromite/start_cromite.sh %u" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Terminal=false" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Type=Application" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Icon=/usr/share/icons/hicolor/48x48/apps/cromite.png" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Comment=Browse the World Wide Web" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Keywords=Internet;WWW;Browser;Web;Explorer" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "Categories=GNOME;GTK;Network;WebBrowser;" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "StartupNotify=true" | tee -a "/usr/share/applications/cromite.desktop"'
-		echo $password | sudo -S 'echo "StartupWMClass=Chromium-browser" | tee -a "/usr/share/applications/cromite.desktop"'
+		echo $password | echo "#!/bin/sh" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo '' | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "# Author:      SAEOS, saeos [dot] com" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "# Description: Cromite is a Chromium fork based on Bromite with built-in support for adblocking and an eye for privacy." | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "# Executed by: user, on-demand" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "# Resources:   /bin/bash, default Ubuntu icon set" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "#" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "[Desktop Entry]" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Name=Cromite" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "X-MultipleArgs=false" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Exec=/usr/bin/cromite/start_cromite.sh %u" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Terminal=false" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Type=Application" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Icon=/usr/share/icons/hicolor/48x48/apps/cromite.png" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Comment=Browse the World Wide Web" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Keywords=Internet;WWW;Browser;Web;Explorer" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "Categories=GNOME;GTK;Network;WebBrowser;" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "StartupNotify=true" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
+		echo $password | echo "StartupWMClass=Chromium-browser" | sudo -S tee -a "/usr/share/applications/cromite.desktop"
             
             
 		#set desktop file permissions
@@ -93,14 +94,14 @@ if [ "$(lsb_release -s -i)" = "Ubuntu" ]; then
         	if [ "$(lsb_release -s -r)" = "24.04" ]; then
         	
 			echo $password | sudo -S touch "/etc/apparmor.d/usr.bin.cromite.chrome"
-			echo $password | sudo -S 'echo "abi <abi/4.0>," | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "include <tunables/global>" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "profile cromite /usr/bin/cromite/chrome flags=(unconfined) {" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "userns," | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "include if exists <local/chrome>" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
-			echo $password | sudo -S 'echo "}" | tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"'
+			echo $password | echo "abi <abi/4.0>," | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "include <tunables/global>" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "profile cromite /usr/bin/cromite/chrome flags=(unconfined) {" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "userns," | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "include if exists <local/chrome>" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
+			echo $password | echo "}" | sudo -S tee -a "/etc/apparmor.d/usr.bin.cromite.chrome"
 			
 			echo $password | sudo -S apparmor_parser -r /etc/apparmor.d/usr.bin.cromite.chrome
             
